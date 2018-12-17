49800b22cfdaf0474073d2e1fdb00c68f3db529c
helloavery <25335417+helloavery@users.noreply.github.com>
2018-12-16 22:00:39 -0500

    Renaming files

--- src/main/java/com/averygrimes/s3gateway/launch/S3GatewayApplicationLaunch.java
+++ src/main/java/com/averygrimes/s3gateway/launch/S3GatewayApplicationLaunch.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class S3GatewayApplicationLaunch {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(S3GatewayApplicationLaunch.class, args);
 	}
 
 }
--- src/test/java/com/averygrimes/s3gateway/S3GatewayApplicationLaunchTests.java
+++ src/test/java/com/averygrimes/s3gateway/S3GatewayApplicationLaunchTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GatewayApplicationTests {
+public class S3GatewayApplicationLaunchTests {
 
 	@Test
 	public void contextLoads() {
