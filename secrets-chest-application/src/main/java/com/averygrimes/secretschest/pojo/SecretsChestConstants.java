6010bb383b9c4623e2b97ae6328ada90a1773f01
helloavery <25335417+helloavery@users.noreply.github.com>
2020-06-13 20:43:21 -0400

    Delete unused enums and constants

--- secrets-chest-application/src/main/java/com/averygrimes/secretschest/pojo/SecretsChestConstants.java
+++ secrets-chest-application/src/main/java/com/averygrimes/secretschest/pojo/SecretsChestConstants.java
@@ -11,10 +11,6 @@ public interface SecretsChestConstants {
     /** SecretsChestsBase Constants **/
     String PLAIN_TEXT_DATA = "plaintext";
 
-    /** AWS Constants **/
-    String AWS_SERVICE_NAME = "execute-api";
-    String AWS_DEFAULT_PROFILE_NAME = "default";
-
     /** Encrypted and Data Map Constants **/
     String ENCRYPTED_DATA_MAP_KEY = "encryptedData";
     String ENCRYPTED_KEY_MAP_KEY= "encryptedKey";
